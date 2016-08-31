8f403083b29eff007b2a6a45c9f7077af76bb133
Vincent <vincentnewkirk@gmail.com>
2016-08-31 10:53:44 -1000

    some webpack config set up and entry js set up

---
+++ entry.jsx
@@ -0,0 +1,17 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestBox from './App.jsx';
+import {createStore, combineReducers} from 'redux';
+import * as reducers from './reducers';
+import {Provider} from 'react-redux';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store} >
+    <TestBox />
+  </Provider>,
+  document.getElementById('content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, '/entry.jsx'),
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+    new ExtractTextPlugin('[name]-[hash].min.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  module: {
+    loaders: [{
+      test: /(\.js$|\.jsx$)/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015'],
+      },
+    }, {
+      test: /\.json?$/,
+      loader: 'json',
+    }, {
+      test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
+      loader: 'file',
+    }, {
+      test: /\.(mp4|webm)$/,
+      loader: 'url?limit=10000'
+    }, {
+      test: /(\.scss$|\.css$)/,
+      loaders: [
+        'style',
+        'css',
+        'sass',
+      ],
+    }],
+    postcss: [
+    require('autoprefixer')
+    ]
+  }
+};
