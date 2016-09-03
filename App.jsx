55d2a8c045754b2f7e6ca15abd64ccef766df4ec
Vincent <vincentnewkirk@gmail.com>
2016-09-03 09:39:37 -1000

    add proof of concept fade-in animation on landing page

--- App.jsx
+++ App.jsx
@@ -1,14 +1,17 @@
 'use strict';
 import React from 'react';
 import { Link } from 'react-router';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class MainButtonBox extends React.Component {
   render(){
     return (
       <div id='main-button-container'>
-        <div id='about-button'><Link to='/about'>About</Link></div>
-        <div id='projects-button'><Link to='/projects'>Projects</Link></div>
-        <div id='contact-button'><Link to='/contact'>Contact</Link></div>
+        <ReactCSSTransitionGroup transitionName="main-button-animation" transitionAppear={true} transitionAppearTimeout={500}>
+          <div id='about-button'><Link to='/about'>About</Link></div>
+          <div id='projects-button'><Link to='/projects'>Projects</Link></div>
+          <div id='contact-button'><Link to='/contact'>Contact</Link></div>
+        </ReactCSSTransitionGroup>
       </div>
     )
   }
