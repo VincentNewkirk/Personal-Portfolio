b45f95df98a2f411a608fa7afc7bb3e7e308c09c
Vincent <vincentnewkirk@gmail.com>
2016-09-02 23:38:50 -1000

    complete writing router to link paths for about project and contact button

--- entry.jsx
+++ entry.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainButtonBox from './App.jsx';
 import about from './portfolioPages/about.jsx';
+import projects from './portfolioPages/projects.jsx';
+import contact from './portfolioPages/contact.jsx';
 import style from "./scss/styles.scss";
 import { Router, Route, hasHistory } from 'react-router';
 
@@ -14,6 +16,8 @@ ReactDOM.render(
   <Router history={hasHistory}>
     <Router path='/' component={MainButtonBox} />
     <Router path='/about' component={about} />
+    <Router path='/projects' component={projects} />
+    <Router path='/contact' component={contact} />
   </Router>,
   document.getElementById('content')
 );
