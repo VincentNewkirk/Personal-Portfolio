80d5bd1cd9c2af6b975c577aea683240cd61e81b
Vincent <vincentnewkirk@gmail.com>
2016-09-05 20:52:21 -1000

    slide out side menu implemented and routing reconfigured and working properly

--- portfolioPages/sideButtons.jsx
+++ portfolioPages/sideButtons.jsx
@@ -2,18 +2,42 @@
 import React from 'react';
 import { Link } from 'react-router';
 import HeaderButton from './header-buttons.jsx';
+import Radium from 'radium';
+const Menu = require('react-burger-menu').bubble;
 
-class SideButtons extends React.Component{
-  render () {
+let RadiumLink = Radium(Link);
+
+
+const SideButtons = React.createClass({
+  // showSettings: function(event) {
+  //   event.preventDefault();
+  //   .
+  //   .
+  //   .
+  // },
+  render: function() {
     return (
-      <div id='main-header'>
-        <HeaderButton linkPath='/about' title='About' />
-        <HeaderButton linkPath='/projects' title='Projects' />
-        <HeaderButton linkPath='/skills' title='Skills' />
-        <HeaderButton linkPath='/contact' title='Contact' />
-      </div>
-    )
+      <Menu width={200} isOpen >
+        <RadiumLink id="about" className="menu-item" to="/about">About</RadiumLink>
+        <RadiumLink id="projects" className="menu-item" to="/projects">Projects</RadiumLink>
+        <RadiumLink id='skills' className="menu-item" to="/skills">Skills</RadiumLink>
+        <RadiumLink id="contact" className="menu-item" to="/contact">Contact</RadiumLink>
+      </Menu>
+    );
   }
-}
+});
+
+// class SideButtons extends React.Component{
+//   render () {
+//     return (
+//       <div id='main-header'>
+//         <HeaderButton linkPath='/about' title='About' />
+//         <HeaderButton linkPath='/projects' title='Projects' />
+//         <HeaderButton linkPath='/skills' title='Skills' />
+//         <HeaderButton linkPath='/contact' title='Contact' />
+//       </div>
+//     )
+//   }
+// }
 
 export default SideButtons;
