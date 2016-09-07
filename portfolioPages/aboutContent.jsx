0af73c909729dd6b11eaf70ab31e5ce8568ff2bc
Vincent <vincentnewkirk@gmail.com>
2016-09-06 17:26:17 -1000

    complete writing tests for about-content component

--- portfolioPages/aboutContent.jsx
+++ portfolioPages/aboutContent.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const AboutContent = () => (
   <div id='about-content'>
     <h1>Vincent Newkirk</h1>
-    <p>I am a full-stack Javascript Developer born and raised in Honolulu, Hawaii. <br/>
+    <p id='bio-desc'>I am a full-stack Javascript Developer born and raised in Honolulu, Hawaii. <br/>
     I am a quick learner and love working with exciting technologies such as React and Angular. <br/>
     I have a passion for making high performance, beautiful web applications.<br/>
     </p>
--- test/App.spec.js
+++ test/App.spec.js
@@ -6,9 +6,22 @@ import SideButtons from '../portfolioPages/sideButtons.jsx';
 import AboutContent from '../portfolioPages/aboutContent';
 
 describe('<AboutContent />', () => {
+  const wrapper = shallow(<AboutContent />)
+  it('Should render a div', () => {
+    expect(wrapper.type()).to.equal('div')
+  });
+  it('The rendered div should have an id of "about-content"', () => {
+    expect(wrapper.find('#about-content')).to.have.length(1)
+  })
   it('Should Render my name', () => {
-    const wrapper = shallow(<AboutContent />)
-    expect(wrapper.contains('Vincent Newkirk')).to.be.true;
+    expect(wrapper.contains('Vincent Newkirk')).to.be.true
+  })
+  it('Should render an h1 tag with my name', () => {
+    expect(wrapper.containsMatchingElement(<h1>Vincent Newkirk</h1>))
+    .to.equal(true)
+  })
+  it('Should render a <p> tag with id of "bio-desc"', () => {
+    expect(wrapper.find('#bio-desc').type()).to.equal('p')
   });
 })
 
