e38a0f03f25fbd9b51f5320886b7f896bd8a27e8
Vincent <vincentnewkirk@gmail.com>
2016-09-10 19:43:18 -1000

    add skills to skillsContent

--- portfolioPages/skills-content.jsx
+++ portfolioPages/skills-content.jsx
@@ -6,7 +6,18 @@ const SkillsContent = () => (
   <div id='skills-content'>
     <h1>Techincal Skills</h1>
     <div>
-      List of Proficiencies:
+      <h3>Front-End</h3>
+      <span>React Angular ES6 D3 Leaflet Sass CSS3 HTML5</span>
+
+      <h3>Server-Side</h3>
+      <span>NodeJS Express MongoDB PostgresQL GraphQL Jade ORM</span>
+
+      <h3>Workflow/Testing Utilities</h3>
+      <span>Git Webpack Gulp Mocha Chai Enzyme Supertest</span>
+
+      <h3>Web Skills</h3>
+      <span>Responsive/Mobile First Development, Test/Behavior Driven Development,
+      Relational and Non-Relational Database Modeling, User Authentication, API Integration</span>
     </div>
    </div>
 )
