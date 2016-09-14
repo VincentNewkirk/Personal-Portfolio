f2b086c7948091aca254514ad3a875ada1fa0ee3
Vincent <vincentnewkirk@gmail.com>
2016-09-14 12:45:52 -1000

    add placeholder images for project listing page and size images

--- portfolioPages/projects-content.jsx
+++ portfolioPages/projects-content.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router';
+import style from '../scss/projectThumbnail.scss';
 
 
 class ProjectContent extends React.Component{
   render(){
     return(
       <div id='project-content' className='content-box'>
-        <div id='xtinct-box'>
+        <div id='xtinct-box' className='project-box'>
           <Link to='/xtinct'>
             <div className='project-thumbnail-box'>
+            <img className='project-thumbnail-img'
+            src='http://feelgrafix.com/data/landscape/landscape-1.jpg' />
             Xtinct.online
             <span className='project-thumbnail-desc'></span>
             </div>
           </Link>
+        </div>
+        <div id='kanban-box' className='project-box'>
           <Link to='/kanban'>
             <div className='project-thumbnail-box'>
+            <img className='project-thumbnail-img'
+            src='http://feelgrafix.com/data/landscape/landscape-1.jpg' />
             React Kanban
             <span className='project-thumbnail-desc'></span>
             </div>
           </Link>
+        </div>
+        <div id='express-box' className='project-box'>
           <Link to='/express'>
             <div className='project-thumbnail-box'>
+            <img className='project-thumbnail-img'
+            src='http://feelgrafix.com/data/landscape/landscape-1.jpg' />
             Express Gallery
             <span className='project-thumbnail-desc'></span>
             </div>
