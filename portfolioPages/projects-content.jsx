b30e9ccceea22e80dfabeec2e0cf4657931fbee5
Vincent <vincentnewkirk@gmail.com>
2016-09-14 15:46:01 -1000

    add header to project list divs and style

--- portfolioPages/projects-content.jsx
+++ portfolioPages/projects-content.jsx
@@ -8,6 +8,7 @@ const ProjectContent = () => (
     <div id='xtinct-box' className='project-box'>
       <Link to='/xtinct'>
         <div className='project-thumbnail-box'>
+        <span className='project-header'>Xtinct.online</span>
         <img className='project-thumbnail-img'
         src='http://i.imgur.com/j7B7Ap4.png' />
         <span className='project-thumbnail-desc'>An interactive map visualizing animal extinction rates</span>
@@ -17,6 +18,7 @@ const ProjectContent = () => (
     <div id='kanban-box' className='project-box'>
       <Link to='/kanban'>
         <div className='project-thumbnail-box'>
+        <span className='project-header'>React Kanban</span>
         <img className='project-thumbnail-img'
         src='http://i.imgur.com/hKfFIm1.png' />
         <span className='project-thumbnail-desc'>Kanban Board using React and Redux</span>
@@ -26,6 +28,7 @@ const ProjectContent = () => (
     <div id='express-box' className='project-box'>
       <Link to='/express'>
         <div className='project-thumbnail-box'>
+        <span className='project-header'>Express Gallery</span>
         <img className='project-thumbnail-img'
         src='http://i.imgur.com/o9ZR7Tb.png' />
         <span className='project-thumbnail-desc'>Custom CMS with password encryption</span>
